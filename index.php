4d86750442ca93a4034dee9fe116843e6516f301
Asare Bright <Asare Bright>
2016-11-26 00:07:33 +0000

    index page created

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<link rel="stylesheet" href="css/font-awesome.css">
+</head>
+
+<body>
+<div id="main"></div>
+<div id="footer"></div>
+</body>
+</html>
