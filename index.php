79b11a9f234bbe67bcdebf8558ec9da68e40cd5e
BHRYT\Bhryt <asarebright81@gmail.com>
2016-12-03 08:08:34 +0000

    index page for the weather bot

--- index.php
+++ index.php
@@ -1,12 +1,15 @@
 <!DOCTYPE html>
 <html>
 <head>
-<link rel="stylesheet" type="text/css" href="css/style.css">
+<link rel="stylesheet" type="text/css" href="css/main.css">
 <link rel="stylesheet" href="css/font-awesome.css">
 </head>
 
 <body>
-<div id="main"></div>
-<div id="footer"></div>
+<div id="main">
+	<div id="act">
+		<div id="activate">activate me!!!</div>
+	</div>
+</div>
 </body>
 </html>
